6f06b9319a7d0036f8580293be2bcd62731494ab
Yousef Mohammed <yousef.abzaby@gmail.com>
2024-12-26 00:23:45 +0200

    starting with OOP

---
+++ P1/src/AnimeChar.java
@@ -0,0 +1,16 @@
+public class AnimeChar {
+
+
+    String name = "Lofi";
+    int age = 18;
+    String title = "Pirate";
+
+
+    void puch() {
+        System.out.println(name + " punched an enemy");
+    }
+
+    void eat(String name){
+        System.out.println(name + " is eating");
+    }
+}
--- P1/src/Main.java
+++ P1/src/Main.java
@@ -1,15 +1,7 @@
-import java.util.ArrayList;
-
-class test {
-    public static void main(String[] arg){
-
-        ArrayList<String> food = new ArrayList();
-        food.add("Apple");
-        food.add("Banana");
-        food.add("Bread");
-
-        for(int i = 0; i<food.size(); i++){
-            System.out.println(food.get(i));
-        }
+public class Main {
+    public static void main(String[] args){
+        AnimeChar myChar = new AnimeChar();
+        System.out.println(myChar.name);
+        myChar.puch();
     }
 }
