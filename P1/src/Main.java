c48f5cb40c7cee7de2669504fbeb4dbcf366a4ee
Yousef Mohammed <yousef.abzaby@gmail.com>
2025-01-20 03:13:12 +0200

    wav file player'

--- P1/src/Main.java
+++ P1/src/Main.java
@@ -1,8 +1,20 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        World onePiece = new World("onePiece");
-        AnimeChar Lofi = new AnimeChar(onePiece.worldName, "Lofi");
-        onePiece.display();
-        Lofi.display();
+    public static void main(String[] args) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("hi.wav");
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+
+
+        String input = scanner.next();
     }
 }
